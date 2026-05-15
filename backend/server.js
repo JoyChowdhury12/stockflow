5eea47408010fa47d52cadbfaa06f46bd0d91463
Joy Chowdhury <jroy2181@gmail.com>
2026-05-16 04:26:23 +0600

    Fixed SMTP transport

--- backend/server.js
+++ backend/server.js
@@ -25,11 +25,15 @@ const pool = new Pool({
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
